564a4d8864ddf7dc905da13374364d29f88c9fb9
itsahnafarafat <ahnafarafat.py@gmail.com>
2025-06-14 13:09:25 +0000

    flask job finder backend

--- job_finder/app/routes/public.py
+++ job_finder/app/routes/public.py
@@ -3,7 +3,7 @@ from app.models import Job
 
 public_bp = Blueprint('public', __name__)
 
-@public_bp.route('/jobs')
-def job_list():
-    jobs = Job.query.all()
-    return render_template('public/job_list.html', jobs=jobs)
+@public_bp.route('/')
+def home():
+    jobs = Job.query.order_by(Job.posted_on.desc()).all()
+    return render_template('public/home.html', jobs=jobs)
